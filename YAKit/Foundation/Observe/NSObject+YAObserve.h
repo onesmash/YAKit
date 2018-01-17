fb71c79055a3926da3277521187b47453eb0ab2f
徐晖 <xuhui@xuhuideMac-mini.local>
2018-01-17 15:07:56 +0800

    release0.0.6

--- YAKit/Foundation/Observe/NSObject+YAObserve.h
+++ YAKit/Foundation/Observe/NSObject+YAObserve.h
@@ -12,6 +12,7 @@ typedef void(^YAObserveHandler)(id observer, id object);
 @protocol YAObserveProtocol <NSObject>
 
 - (void)ya_addObserver:(NSObject *)observer forKey:(NSString *)key block:(YAObserveHandler)block;
+- (void)ya_addObserverOnce:(NSObject *)observer forKey:(NSString *)key block:(YAObserveHandler)block;
 - (void)ya_removeObserver:(NSObject *)observer forKey:(NSString *)key;
 - (void)ya_removeObserver:(NSObject *)observer;
 
@@ -31,4 +32,16 @@ _Pragma("clang diagnostic pop") \
 }
 #else
 #define YAObserve(object, key, handler) [object ya_addObserver:self forKey:@#key block:(handler)];
+#endif
+
+#if DEBUG
+#define YAObserveOnce(object, key, handler) [object ya_addObserverOnce:self forKey:@#key block:(handler)]; \
+{ \
+_Pragma("clang diagnostic push") \
+_Pragma("clang diagnostic ignored \"-Wunused-getter-return-value\"") \
+object.key; \
+_Pragma("clang diagnostic pop") \
+}
+#else
+#define YAObserveOnce(object, key, handler) [object ya_addObserverOnce:self forKey:@#key block:(handler)];
 #endif
--- YAKit/Foundation/Observe/NSObject+YAObserve.m
+++ YAKit/Foundation/Observe/NSObject+YAObserve.m
@@ -33,6 +33,33 @@ static char kObserverKey;
     return lock_;
 }
 
+- (void)ya_addObserverOnce:(NSObject *)observer forKey:(NSString *)key block:(YAObserveHandler)block
+{
+    SEL sel = NSSelectorFromString(key);
+    if([self respondsToSelector:sel]) {
+        [observer.KVOController observe:self keyPath:key options:0 block:^(NSObject *observer, id object, NSDictionary *change) {
+            [observer.KVOController unobserve:object keyPath:key];
+            if(block) {
+                block(observer, object);
+            }
+        }];
+    } else {
+        [self.lock lock];
+        NSMapTable *table = [self.observers objectForKey:key];
+        if(!table) {
+            table = [NSMapTable weakToStrongObjectsMapTable];
+            [self.observers setObject:table forKey:key];
+        }
+        NSMutableArray *blocks = [table objectForKey:observer];
+        if(!blocks) {
+            blocks = [NSMutableArray array];
+            [table setObject:blocks forKey:observer];
+        }
+        [blocks addObject:block];
+        [self.lock unlock];
+    }
+}
+
 - (void)ya_addObserver:(NSObject *)observer forKey:(NSString *)key block:(YAObserveHandler)block
 {
     SEL sel = NSSelectorFromString(key);
