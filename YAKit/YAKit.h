7cf85a7a6329ca341d4da0315a7c1aeb5e0b0df5
徐晖 <xuhui@xuhuideMac-mini.local>
2018-03-12 11:07:11 +0800

    misc

--- YAKit/YAKit.h
+++ YAKit/YAKit.h
@@ -9,11 +9,26 @@
 #import <Foundation/Foundation.h>
 #import "NSObject+YAComponent.h"
 #import "NSObject+YAObserve.h"
-#import "YAViewPresenterProtocol.h"
-#import "YAVCPresenterProtocol.h"
 #import "YAComponent.h"
 #import "YAModel.h"
 #import "NSString+YA.h"
 
-
-
+#pragam mark - MVP
+#import "YAMVPProtocol.h"
+#import "YARoutingProtocol.h"
+#import "YAScrollViewPresenterProtocol.h"
+#import "YAViewPresenterProtocol.h"
+#import "YAVCPresenterProtocol.h"
+#import "YACellPresenterProtocol.h"
+#import "YACellPresenterRequiredProtocol.h"
+#import "YACollectionCellPresenter.h"
+#import "YACollectionVCPresenter.h"
+#import "YACollectionViewPresenter.h"
+#import "YACollectionViewPresenterProtocol.h"
+#import "YATableCellPresenter.h"
+#import "YATableVCPresenter.h"
+#import "YATableViewPresenter.h"
+#import "YATableViewPresenterProtocol.h"
+#import "YAVCPresenterProtocol.h"
+#import "YAViewPresenter.h"
+#import "YAViewPresenterProtocol.h"
